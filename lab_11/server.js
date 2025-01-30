f796e742167685ac81afd8dd1addd298134904d5
Dawn-com <2327296@rgu.ac.uk>
2025-01-30 11:37:28 +0000

    stuff

--- lab_11/server.js
+++ lab_11/server.js
@@ -10,8 +10,8 @@ app.get('/joke', function(req, res){
 res.send('What is a bee in the us called? a USB');
 });
 app.get('/add', function(req, res){
-    var x = req.query.x;
-    var y = req.query.y;
-     res.send('X + Y='+(x+y));
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+    res.send('X + Y='+(x+y));
 });
 app.listen(8080);
