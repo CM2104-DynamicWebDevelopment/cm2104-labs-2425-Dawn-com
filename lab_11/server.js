7f3a6c9a5676847cee14486d1dfc5fc8124ecd2d
Dawn-com <2327296@rgu.ac.uk>
2025-01-30 12:16:37 +0000

    huuhsuo

--- lab_11/server.js
+++ lab_11/server.js
@@ -14,5 +14,10 @@ app.get('/add', function(req, res){
     var y = parseInt(req.query.y);
     res.send('X + Y='+(x+y));
 });
+app.get('/getform', function(req, res){
+    var name = req.query.name;
+    var quest = req.query.quest;
+    res.send("Hi "+name+" I am sure you will "+quest) ;
+});
 app.use(express.static('public'))
 app.listen(8080);
